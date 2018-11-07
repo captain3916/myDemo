fe0b150966ee3af327cc94e966f8c558d8756a64
captain39166 <guoningdong2@163.com>
2018-11-08 00:33:02 +0800

    实现了轮播图功能

--- huawei/js/index.js
+++ huawei/js/index.js
@@ -4,6 +4,43 @@ $(function () {
         $("#advert").hide();
     });
 
+    //主页轮播
+    ; (function () {
+        var banIndex = 0;
+        var banImg = $("#banner>.ec-slider>.ec-slider-img>li");
+        var banNav = $("#banner>.ec-slider>.ec-slider-nav-1>li");
+        //定时器
+        var banTimer = setInterval(showNext, 2000);
+
+        function showNext() {
+            banIndex = ++banIndex % 9;
+            showImg();
+        };
+
+        function showImg() {
+            banImg.eq(banIndex).fadeIn(500).siblings().fadeOut(500);
+            banNav.eq(banIndex).addClass("current").siblings().removeClass("current");
+        };
+
+        //鼠标移入移出(导航)
+        banNav.hover(function () {
+            clearInterval(banTimer);
+            banIndex = $(this).index();
+            showImg();
+        }, function () {
+            banTimer = setInterval(showNext, 2000);
+        });
+
+        //鼠标移入移出(图片)
+        banImg.hover(function () {
+            clearInterval(banTimer);
+        }, function () {
+            banTimer = setInterval(showNext, 2000);
+        });
+    })();
+
+
+
 
 
 });
